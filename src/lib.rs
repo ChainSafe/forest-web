09039f1cc1db37da0fa910992e1c79baa927e1fc
David Himmelstrup <david.himmelstrup@chainsafe.io>
2024-07-19 13:48:06 +0200

    feat: hardcode rpc calls for network name and version (#6)

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,101 @@
-use leptos::{component, view, IntoView};
+use leptos::{component, create_local_resource, event_target_value, view, IntoView, SignalGet};
 use leptos_meta::*;
+
+use leptos::*;
+
+use serde_json::{json, Value};
+
+const GLIF_CALIBNET: &'static str = "https://api.calibration.node.glif.io";
+
+#[component]
+pub fn Loader(loading: impl Fn() -> bool + 'static) -> impl IntoView {
+    view! {
+        <span class:loader=move || loading()/>
+    }
+}
 
 #[component]
 pub fn App() -> impl IntoView {
     provide_meta_context();
+    let rpc_provider = create_rw_signal(String::from(GLIF_CALIBNET));
+    let network_name = create_local_resource(
+        move || rpc_provider.get(),
+        move |provider| async move {
+            let client = reqwest::Client::new();
+            let res = client
+                .post(provider)
+                .json(&json! {
+                    {
+                        "jsonrpc": "2.0",
+                        "method": "Filecoin.StateNetworkName",
+                        "params": [],
+                        "id": 0
+                    }
+                })
+                .send()
+                .await
+                .ok()?;
+            log::info!("Got response: {:?}", res);
+            Some(String::from(
+                res.json::<Value>()
+                    .await
+                    .ok()?
+                    .get("result")
+                    .cloned()?
+                    .as_str()?,
+            ))
+        },
+    );
+
+    let network_version = create_local_resource(
+        move || rpc_provider.get(),
+        move |provider| async move {
+            let client = reqwest::Client::new();
+            let res = client
+                .post(provider)
+                .json(&json! {
+                    {
+                        "jsonrpc": "2.0",
+                        "method": "Filecoin.StateNetworkVersion",
+                        "params": [[]],
+                        "id": 0
+                    }
+                })
+                .send()
+                .await
+                .ok()?;
+            log::info!("Got response: {:?}", res);
+            res.json::<Value>()
+                .await
+                .ok()
+                .unwrap_or_default()
+                .get("result")
+                .cloned()
+                .unwrap_or_default()
+                .as_u64()
+        },
+    );
+
     view! {
         <Stylesheet href="/pkg/style.css"/>
         <Link rel="icon" type_="image/x-icon" href="/pkg/favicon.ico"/>
-        <p>Forest Explorer</p>
+        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Forest Explorer</h1>
+        <select on:change=move |ev| {
+            rpc_provider.set(event_target_value(&ev))
+        }>
+            <option value="https://api.calibration.node.glif.io">Glif.io Calibnet</option>
+            <option value="https://api.node.glif.io/">Glif.io Mainnet</option>
+        </select>
+        <p>StateNetworkName</p>
+        <p class="px-8">
+            <span>{move || network_name.get() }</span>
+            <Loader loading={move || network_name.loading().get() }/>
+        </p>
+        <p>StateNetworkVersion</p>
+        <p class="px-8">
+            <span>{move || network_version.get() }</span>
+            <Loader loading={move || network_name.loading().get() }/>
+        </p>
     }
 }
 
